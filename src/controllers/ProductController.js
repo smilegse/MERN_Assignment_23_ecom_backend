4402b3d2c9bd8d121aaf90d76c506381e3a776d9
Md. Abu Bakar Siddique <abms.siddique@gmail.com>
2024-05-29 16:48:11 +0600

    Product and user end point done

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,8 +1,60 @@
+const {BrandListService,CategoryListService,
+    SliderListService,ListByCategoryService,ListByBrandService,ListByRemarkService,
+    ListBySmilierService,ListByKeywordService,
+    DetailsService,CreateReviewService,ReviewListService} = require('../services/ProductServices');
 
-const {BrandListService} = require('../services/ProductServices')
-
-exports.ProductListByBrand=async(req,res)=>{
+exports.ProductBrandList=async(req,res)=>{
     let result = await BrandListService(req);
     return res.status(200).json(result)
 }
 
+exports.ProductCategoryList=async(req,res)=>{
+    let result = await CategoryListService();
+    return res.status(200).json(result)
+}
+
+exports.ProductSliderList=async(req,res)=>{
+    let result = await SliderListService();
+    return res.status(200).json(result)
+}
+
+exports.ProductListByBrand=async(req,res)=>{
+    let result = await ListByBrandService(req);
+    return res.status(200).json(result)
+}
+
+exports.ProductListByCategory=async(req,res)=>{
+    let result = await ListByCategoryService(req);
+    return res.status(200).json(result)
+}
+
+exports.ProductListBySmilier=async(req,res)=>{
+    let result = await ListBySmilierService(req);
+    return res.status(200).json(result)
+}
+
+exports.ProductListByKeyword=async(req,res)=>{
+    let result = await ListByKeywordService(req);
+    return res.status(200).json(result)
+}
+
+exports.ProductListByRemark=async(req,res)=>{
+    let result = await ListByRemarkService(req);
+    return res.status(200).json(result)
+}
+
+
+exports.ProductDetails=async(req,res)=>{
+    let result = await DetailsService(req);
+    return res.status(200).json(result)
+}
+
+exports.ProductReviewList=async(req,res)=>{
+    let result = await ReviewListService(req);
+    return res.status(200).json(result)
+}
+
+exports.CreateReview=async(req,res)=>{
+    let result = await CreateReviewService(req);
+    return res.status(200).json(result)
+}
--- src/route/api.js
+++ src/route/api.js
@@ -1,11 +1,40 @@
 const express = require('express');
-const ProductController=require('../controllers/ProductController')
-
+const ProductController = require('../controllers/ProductController')
+const UserController = require('../controllers/UserController');
+const AuthVerification = require('../middlewares/AuthVerification');
 
 
 const router = express.Router();
 
 // Product
-router.get('/ProductBrandList',ProductController.ProductListByBrand)
+router.get('/ProductBrandList',ProductController.ProductBrandList);
+router.get('/ProductCategoryList',ProductController.ProductCategoryList);
+router.get('/ProductSliderList',ProductController.ProductSliderList)
+
+router.get('/ProductListByBrand/:BrandID',ProductController.ProductListByBrand)
+router.get('/ProductListByCategory/:CategoryID',ProductController.ProductListByCategory)
+router.get('/ProductListBySmilier/:CategoryID',ProductController.ProductListBySmilier)
+router.get('/ProductListByKeyword/:Keyword',ProductController.ProductListByKeyword)
+router.get('/ProductListByRemark/:Remark',ProductController.ProductListByRemark)
+router.get('/ProductDetails/:ProductID',ProductController.ProductDetails)
+router.get('/ProductReviewList/:ProductID',ProductController.ProductReviewList)
+
+// User
+router.get('/UserOTP/:email',UserController.UserOTP)
+router.get('/VerifyLogin/:email/:otp',UserController.VerifyLogin)
+router.get('/UserLogout',AuthVerification,UserController.UserLogout)
+router.post('/CreateProfile',AuthVerification,UserController.CreateProfile)
+router.post('/UpdateProfile',AuthVerification,UserController.UpdateProfile)
+router.get('/ReadProfile',AuthVerification,UserController.ReadProfile)
+
+
+
+
+router.post('/SaveWishList')
+router.post('/RemoveWishList')
+router.get('/WishList')
+router.post('/SaveCartList')
+router.post('/RemoveCartList')
+router.get('/CartList')
 
 module.exports = router;
